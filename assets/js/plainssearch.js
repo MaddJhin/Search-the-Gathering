ed212fc6df7075ccdb774a6a6a617268bbb1e6d4
Francisco Carrera <franciscocarrer@gmail.com>
2017-09-14 18:04:00 -0400

    New Searches clear old search history

--- assets/js/plainssearch.js
+++ assets/js/plainssearch.js
@@ -1,7 +1,7 @@
 var baseURL = "https://api.magicthegathering.io/v1"
 
 function ShowResults(){
-    $('#search-results').css("display", "none");
+    $('#search-results').css("display", "block");
 }
 
 $(document).ready(function(){
@@ -16,19 +16,21 @@ $(document).ready(function(){
             method: 'GET'
         }).done(function(response){
             var result = response.cards;
+            console.log(result);
             if(result.length > 1)
             {
+                console.log("Adding multiple results");
                 var listGroup = $('<div>');
                 listGroup.addClass('list-group');
 
                 for (var i = 0; i < result.length; i++) {
                     var button = $('<button>');
                     button.addClass('list-group-item list-group-item-action')
-                        .text(result.name);
+                        .text(result[i].name);
 
                     listGroup.append(button);
                 }
-                $('#card-multiple').append(listGroup);
+                $('#card-multiple').empty().append(listGroup);
                 ShowResults();
             }
         });
